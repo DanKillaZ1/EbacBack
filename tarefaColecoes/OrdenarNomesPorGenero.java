6a4f8b7d4c0409f501a0c353bd5f4e47621403ec
DanKillaZ1 <nielmarcos@hotmail.com>
2023-07-19 21:13:43 -0300

    refazendo exer2

--- tarefaColecoes/OrdenarNomesPorGenero.java
+++ tarefaColecoes/OrdenarNomesPorGenero.java
@@ -4,17 +4,33 @@ public class OrdenarNomesPorGenero {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite os nomes e gêneros separados por traço (-):");
+        System.out.println("Digite os nomes e gêneros separados por vírgula (ex: Ana-F, Felipe-M):");
         String input = scanner.nextLine();
 
-        String[] dados = input.split("-");
+        String[] dados = input.split(", ");
 
-        List<String> nomes = new ArrayList<>(Arrays.asList(dados));
+        List<String> nomesMulheres = new ArrayList<>();
+        List<String> nomesHomens = new ArrayList<>();
 
-        Collections.sort(nomes);
+        for (String dado : dados) {
+            String[] pessoa = dado.split("-");
+            String nome = pessoa[0];
+            String genero = pessoa[1];
 
-        System.out.println("Nomes em ordem alfabética:");
-        for (String nome : nomes) {
+            if (genero.equalsIgnoreCase("F")) {
+                nomesMulheres.add(nome);
+            } else if (genero.equalsIgnoreCase("M")) {
+                nomesHomens.add(nome);
+            }
+        }
+
+        System.out.println("Nomes de mulheres:");
+        for (String nome : nomesMulheres) {
+            System.out.println(nome);
+        }
+
+        System.out.println("Nomes de homens:");
+        for (String nome : nomesHomens) {
             System.out.println(nome);
         }
     }
